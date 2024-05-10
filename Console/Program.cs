22cf5f42c9a03f4541ecc6750d0002679f589409
Sasha2286969 <alex.panfilenok@gmail.com>
2024-05-10 20:04:04 +0000

    calculator2286969

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,3 +1,14 @@
-﻿Console.WriteLine("Hello. What is your name?");
-var name = Console.ReadLine();
-Console.WriteLine($"Hello, {name}");
+﻿// Console.WriteLine("Hello. What is your name?");
+//   var name = Console.ReadLine();
+// Console.WriteLine($"Hello, {name}");
+// this is a sipmle program
+Console.WriteLine("Enter argument one");
+var argument_one_str = Console.ReadLine();
+var argument_one_dig = int.Parse(argument_one_str);
+Console.WriteLine("Enter argument two");
+var argument_two_str = Console.ReadLine();
+var argument_two_dig = int.Parse(argument_two_str);
+var result = argument_one_dig + argument_two_dig;
+var result_str = result + " This is youre result";
+Console.WriteLine(result_str);
+// hihihiha
